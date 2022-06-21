c6e25cf876f0bed3ba1362fe35b2f9969c692f7c
GHeadFirst <alfaresbakri@gmail.com>
2022-06-21 20:04:43 +0300

    Refactored the code a bit and started implementing the checkboxes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 // querySelector
 
 const passwordLengthInput = document.querySelector("#password-length");
@@ -8,6 +9,14 @@ const generateBtn = document.querySelector("#generate-button");
 const copyBtn = document.querySelector("#copy-button");
 
 const passwordResult = document.querySelector("#password-result");
+
+const upperCaseInput = document.querySelector("#UPPERCASE");
+console.log(upperCaseInput.checked);
+const lowerCaseInput = document.querySelector("#lowercase");
+const numbersInput = document.querySelector("#special-symbols");
+const symbolsInput = document.querySelector("#numbers-input");
+const excludedCharactersInput = document.querySelector("#excluded-characters");
+console.log(excludedCharactersInput.checked);
 
 generateBtn.addEventListener(
   "click",
@@ -26,7 +35,6 @@ generateBtn.addEventListener(
 //   alert("Copied the text: " + randomPassword.value);
 // });
 
-// original unrandomized arrays
 const upperCase = [
   "A",
   "B",
@@ -61,71 +69,41 @@ const lowerCase = upperCase.map(
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
-// declartives
-let upperCaseRandomized;
-let lowerCaseRandomized;
-let numbersRandomized;
-let symbolsRandomized;
-
-// console.log(lowerCase);
-
-function getRandomUpperCase() {
-  upperCaseRandomized = upperCase.sort(() => 0.5 - Math.random());
-}
-
-function getRandomLowerCase() {
-  lowerCaseRandomized = lowerCase.sort(() => 0.5 - Math.random());
-}
-
-function getRandomNumbers() {
-  numbersRandomized = numbers.sort(() => 0.5 - Math.random());
-}
-
-function getRandomSymbols() {
-  symbolsRandomized = symbols.sort(() => 0.5 - Math.random());
-}
-
-getRandomLowerCase();
-getRandomNumbers();
-getRandomSymbols();
-getRandomUpperCase();
-
-console.log(upperCaseRandomized);
-console.log(lowerCaseRandomized);
-console.log(numbersRandomized);
-console.log(symbolsRandomized);
-
-const allCharacters = [
-  ...upperCaseRandomized,
-  ...lowerCaseRandomized,
-  ...numbersRandomized,
-  ...symbolsRandomized,
-];
-
-console.log(allCharacters);
+// index of A-Z (0,25) / index of a-z (26,51) / index of 0-9 (52,61) / index of symbols (62,69) / index of excluded characters (8,11,14,34,37,52,53)
+// console.log(allCharactersRandomized);
 // The main function
+let allCharacters = [];
+let allCharactersRandomized = [];
 
+console.log(allCharactersRandomized);
 function passwordGenerator(passwordLength) {
+  allCharacters = [
+    upperCaseInput.checked ? upperCase : [],
+    lowerCaseInput.checked ? lowerCase : [],
+    numbersInput.checked ? numbers : [],
+    symbolsInput.checked ? symbols : [],
+  ].flat();
+
+  allCharactersRandomized = [...allCharacters].sort(() => 0.5 - Math.random());
+
   passwordLength = passwordLengthInput.value;
-  const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=.*\d).*$/;
+  const regexDefault = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=.*\d).*$/;
+  let regexWithoutSymbols;
+  let regexWithoutNumbers;
+  let regexWithoutLower;
+  let regexWithoutUpper;
   let randomPassword;
   let passwordArray = [];
   for (let i = 0; i < passwordLength; i++) {
-    let temp = allCharacters[Math.floor(Math.random() * 70)];
+    let temp =
+      allCharactersRandomized[
+        Math.floor(Math.random() * allCharactersRandomized.length)
+      ];
     passwordArray.push(temp);
+    // console.log(passwordArray);
   }
   randomPassword = passwordArray.join("");
   if (randomPassword.match(/^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=.*\d).*$/))
     return randomPassword;
-  else {
-    // console.log(passwordLength);
-    return passwordGenerator(passwordLength);
-  }
+  else return passwordGenerator(passwordLength);
 }
-
-// console.log(passwordGenerator());
-// console.log(passwordGenerator().length);
-// console.log(randomPassword);
-
-// Connecting to front-end
-// console.log(passwordLengthInput.value);
